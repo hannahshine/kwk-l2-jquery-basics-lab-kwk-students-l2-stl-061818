112171c2a7cbdcc4e478c007cc680503cac35549
Hannah Shine <hshine@visitationacademy.org>
2018-06-22 20:48:22 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -1,6 +1,6 @@
 //add your code here
 let num1=$('#number1');
 let operater=$('#operation');
-let num2=$('#number12');
+let num2=$('#number2');
 
-function doMath(num1,operater,num2){
+function doMath(num1,operater,num2){}
