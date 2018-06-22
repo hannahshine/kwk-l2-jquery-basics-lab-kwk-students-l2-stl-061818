25fab1667256105915773b0a4f7d58dc6e9b13d2
Hannah Shine <hshine@visitationacademy.org>
2018-06-22 21:26:49 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -2,10 +2,14 @@
 let num1=$('#number1');
 let operater=$('#operation');
 let num2=$('#number2');
-function validate(operater,num1,num2){
+function validate(operater,num1,num2)
   if (operater == "+" || operater == "-"||operater=="*"||operater=="/"){
     if (isNaN(num1)||num1===""|| isNaN(num2) || num2 ==="") { $("#result").text("Sorry, one of those is not a valid number!"); }
   else {answer(operater,num1,num2);
     }
   }
-  else 
+}
+  else {
+    $("#result").html("Sorry that is not a valid operator")
+    }
+
